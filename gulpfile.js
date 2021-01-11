2a049983345c66aaf325edf6475c4221e9a91acb
Zvereck20 <kerusov20@gmail.com>
2021-01-11 18:00:57 +1000

    gh

--- gulpfile.js
+++ gulpfile.js
@@ -82,7 +82,7 @@ const sprite = () => {
   return gulp.src("source/img/icons/*.svg")
     .pipe(svgstore())
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("build/img"));
+    .pipe(gulp.dest("build/img/icons"));
 }
 
 exports.sprite = sprite;
